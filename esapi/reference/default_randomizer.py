9e1a4b540d080f23b27c61e5b3716c2dadef1959
cyounkins <cyounkins@localhost>
2009-06-10 12:15:28 -0400

    Implemented getRandomChoice, re-inserted getRandomFloat (whoops), and changed implementation of getRandomGUID.
    Reduced dependencies by using a homebrew getRandomGUID that is RFC 4122 V4 compliant.

--- esapi/reference/default_randomizer.py
+++ esapi/reference/default_randomizer.py
@@ -21,7 +21,9 @@ it will use CryptGenRandom. If an OS level source of entropy is not found, a Not
 will be thrown.
 """
 
-import uuid
+# Todo
+# Change alpha_numerics and hex once Encoder is written
+
 from random import SystemRandom
 
 import esapi.core
@@ -29,7 +31,8 @@ from esapi.logger import Logger
 from esapi.randomizer import Randomizer
 
 class DefaultRandomizer(Randomizer):
-    ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alpha_numerics = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    hex = '0123456789abcdef'
 
     def __init__(self):
         self.secureRandom = SystemRandom()
@@ -38,20 +41,34 @@ class DefaultRandomizer(Randomizer):
     def getRandomString(self, length, characterSet):
         ret = ""
         for i in range(length):
-            ret += self.secureRandom.choice(characterSet)
+            ret += self.getRandomChoice(characterSet)
+        return ret
 
     def getRandomBoolean(self):
-        return self.secureRandom.choice([True, False])
+        return self.getRandomChoice([True, False])
 
-    def getRandomNumber(self, min, max):
+    def getRandomInteger(self, min, max):
         return self.secureRandom.randint(min, max)
 
     def getRandomFilename(self, extension):
-        fn = self.getRandomString(12, self.ascii_letters) + "." + extension
+        fn = self.getRandomString(12, self.alpha_numerics) + "." + extension
         self.logger.debug(Logger.SECURITY_SUCCESS, "Generated a new random filename: " + fn)
         return fn
+        
+    def getRandomFloat(self, min, max):
+        return self.secureRandom.uniform(min, max)
 
     def getRandomGUID(self):
-        return str(uuid.uuid4())
+        parts = [None] * 5
+        parts[0] = self.getRandomString(8, self.hex)
+        parts[1] = self.getRandomString(4, self.hex)
+        parts[2] = '4' + self.getRandomString(3, self.hex) # Sets GUID version to 4
+        parts[3] = self.getRandomChoice('89ab') + self.getRandomString(3, self.hex) # Sets high bits
+        parts[4] = self.getRandomString(12, self.hex)
+        return '-'.join(parts)
+                    
+    def getRandomChoice(self, seq):
+        return self.secureRandom.choice(seq)
+        
 
 
