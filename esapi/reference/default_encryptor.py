6de03653b13ee5edd36e644b78e9139279654b2f
Craig Younkins <craig.younkins@owasp.org>
2009-06-10 17:12:03 -0400

    Added working hash method.

--- esapi/reference/default_encryptor.py
+++ esapi/reference/default_encryptor.py
@@ -22,13 +22,15 @@ from esapi.encryptor import Encryptor
 
 class DefaultEncryptor(Encryptor):
     def __init__(self):
-        self.hashAlgorithm = esapi.core.securityConfiguration().getHashAlgorithm();
-        self.hashIterations = esapi.core.securityConfiguration().getHashIterations();
+        self.hashAlgorithm = esapi.core.getSecurityConfiguration().getHashAlgorithm();
+        self.hashIterations = esapi.core.getSecurityConfiguration().getHashIterations();
 
-    def hash(self, plaintext, salt, iterations=self.hashIterations):
+    def hash(self, plaintext, salt, iterations=None):
+        if iterations is None: iterations = self.hashIterations
+    
         try:
             digest = hashlib.new(self.hashAlgorithm)
-            digest.update(esapi.core.securityConfiguration().getMasterSalt())
+            digest.update(esapi.core.getSecurityConfiguration().getMasterSalt())
             digest.update(salt)
             digest.update(plaintext)
             
@@ -38,10 +40,9 @@ class DefaultEncryptor(Encryptor):
                 digest.update(bytes)
                 bytes = digest.digest()
                 
-            #encoded = esapi.core.encoder().encodeForBase64(bytes, false)
-            #return encoded
-            
-            return bytes
+            import base64
+            encoded = base64.b64encode(bytes)
+            return encoded
             
         except ValueError, e:
             raise EncryptionException, "Internal Error - Can't find hash algorithm " + self.hashAlgorithm
