313b5eab343b57feba777f23c2b8d6e54f494623
Craig Younkins <craig.younkins@owasp.org>
2009-06-22 12:59:03 -0400

    Adding PercentCodec

---
+++ esapi/codecs/percent_codec.py
@@ -0,0 +1,86 @@
+"""
+OWASP Enterprise Security API (ESAPI)
+ 
+This file is part of the Open Web Application Security Project (OWASP)
+Enterprise Security API (ESAPI) project. For details, please see
+<a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+Copyright (c) 2009 - The OWASP Foundation
+
+The ESAPI is published by OWASP under the BSD license. You should read and 
+accept the LICENSE before you use, modify, and/or redistribute this software.
+
+@author Craig Younkins (craig.younkins@owasp.org)
+"""
+
+import esapi.codecs.codec
+from esapi.codecs.codec import Codec
+
+class PercentCodec(Codec):
+    """
+    Implementation of the Codec interface for percent encoding (aka URL
+    encoding)
+    """
+    
+    def encode_character(self, immune, char):
+        """
+        Encodes a single character according to the spec at:
+        http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.1
+        Spaces are replaced by '+'. All characters not in immune are escaped
+        as described in http://tools.ietf.org/html/rfc3986#section-2.1 .
+        """
+        
+        if char in immune:
+            return char
+            
+        if char == ' ':
+            return '+'
+        
+        hex_str = esapi.codecs.codec.get_hex_for_non_alphanumeric(char)
+        if hex_str is None:
+            return char
+            
+        # get_hex_for_non_alphanumeric returns '0x3c'
+        # Strip the 0x and upper it
+        hex_str = hex_str[2:].upper() 
+        if ord(char) < 0x10:
+            hex_str = '0' + hex_str
+            
+        return '%' + hex_str
+    
+    def decode_character(self, pbs):
+        """
+        Decodes a single character according to the spec at:
+        http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.1
+        '+' decodes to ' '. All characters not in immune are escaped
+        as described in http://tools.ietf.org/html/rfc3986#section-2.1 .
+        """
+        pbs.mark()
+        
+        first = pbs.next()
+        if first is None:
+            pbs.reset()
+            return None
+           
+        if first == '+':
+            return ' '
+            
+        if first != '%':
+            pbs.reset()
+            return None
+            
+        hex_digits = ''
+        for i in range(2):
+            char = pbs.next_hex()
+            if char is not None: hex_digits += char
+            
+        if len(hex_digits) == 2:
+            try:
+                ret = chr( int( hex_digits, 16 ) )
+                return ret
+            except ValueError:
+                pass
+                # Malformed?
+        
+        pbs.reset()
+        return None
+        
---
+++ esapi/test/codecs/test_percent_codec.py
@@ -0,0 +1,68 @@
+"""
+OWASP Enterprise Security API (ESAPI)
+ 
+This file is part of the Open Web Application Security Project (OWASP)
+Enterprise Security API (ESAPI) project. For details, please see
+<a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+Copyright (c) 2009 - The OWASP Foundation
+
+The ESAPI is published by OWASP under the BSD license. You should read and 
+accept the LICENSE before you use, modify, and/or redistribute this software.
+
+@author Craig Younkins (craig.younkins@owasp.org)
+"""
+
+import unittest
+
+from esapi.codecs.percent_codec import PercentCodec
+
+class EncoderTest(unittest.TestCase):
+
+    known_values = ( ('a', 'a'),
+                     ('d', 'd'),
+                     ('z', 'z'),
+                     ('A', 'A'),
+                     ('M', 'M'),
+                     ('Z', 'Z'),
+                     (' ', '+'),
+                     ('!', '%21'),
+                     ('*', '%2A'),
+                     ("'", '%27'),
+                     ('?', '%3F'),
+                     ('#', '%23'),
+                     ('/', '%2F'),
+                     ('&', '%26'),
+                     ('<script>', '%3Cscript%3E'))
+                     
+    
+    def __init__(self, test_name=""):
+        """
+        Instantiates a new EncoderTest test.
+        
+        @param test_name the test name
+        """
+        unittest.TestCase.__init__(self, test_name)
+        self.codec = PercentCodec()
+        
+    def test_encoding(self):
+        for plain, encoded in self.known_values:
+            result = self.codec.encode('', plain) # No immunes
+            self.assertEquals(encoded, result)
+    
+    def test_decoding(self):
+        for decoded, encoded in self.known_values:
+            result = self.codec.decode(encoded)
+            self.assertEquals(decoded, result)
+            
+    def test_immunes(self):
+        immune = '~_-.'
+        plain = immune[:]
+        result = self.codec.encode(immune, plain)
+        self.assertEquals(result, plain)
+        
+        decoded = self.codec.decode(result)
+        self.assertEquals(decoded, plain)
+        
+    
+if __name__ == "__main__":
+    unittest.main()
