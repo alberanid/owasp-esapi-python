7b7a173633ec362284146a083d1242a91f1c7b17
Craig Younkins <craig.younkins@owasp.org>
2009-07-16 17:04:19 -0400

    Adding User interface and initial DefaultUser implementation

---
+++ esapi/reference/default_user.py
@@ -0,0 +1,398 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@license: OWASP Enterprise Security API (ESAPI)
+     
+    This file is part of the Open Web Application Security Project (OWASP)
+    Enterprise Security API (ESAPI) project. For details, please see
+    U{http://www.owasp.org/index.php/ESAPI<http://www.owasp.org/index.php/ESAPI>}.
+
+    The ESAPI is published by OWASP under the BSD license. You should read and 
+    accept the LICENSE before you use, modify, and/or redistribute this software.
+    
+@copyright: Copyright (c) 2009 - The OWASP Foundation
+@summary: The User interface represents an application user or user account.
+@author: Craig Younkins (craig.younkins@owasp.org)
+"""
+
+# Todo
+# Change many things after authenticator, httpUtilities written
+# Some methods not implemented
+# Safe initial values
+
+from datetime import datetime
+
+from esapi.core import ESAPI
+from esapi.translation import _
+from esapi.user import User
+from esapi.exceptions import AuthenticationAccountsException, AuthenticationLoginExecption, AuthenticationHostException
+from esapi.logger import Logger
+
+
+class DefaultUser(User):
+    """
+    The reference implementation of the User interface. This implementation
+    is pickled/shelved into a flat file.
+    """
+    
+    IDLE_TIMEOUT_LENGTH = ESAPI.security_configuration().get_session_idle_timeout_length()
+    
+    ABSOLUTE_TIMEOUT_LENGTH = ESAPI.security_configuration().get_session_absolute_timeout_length()
+    
+    MAX_ROLE_LENGTH = 250
+    
+    def __init__(self, account_name):
+        """
+        Instantiates a new user.
+        
+        @param account_name: The name of this user's account.
+        """
+        User.__init__(self)
+        
+        self.account_name = account_name
+        
+        # Get random numbers until we find an unused account number
+        while True:
+            id = ESAPI.randomizer().get_random_integer(1)
+            #if ESAPI.authenticator().get_user(id) is None and id != 0:
+            self.account_id = id
+            break
+        
+        self._logger = ESAPI.get_logger("DefaultUser")
+        self._screen_name = None
+        self._csrf_token = self.reset_csrf_token()
+        self._roles = []
+        self._locked = None
+        self._logged_in = None
+        self._enabled = None
+        self._last_host_address = None
+        self._last_password_change_time = None
+        self._last_login_time = None
+        self._last_failed_login_time = None
+        self._expiration_time = None
+        self._sessions = []
+        self._event_map = {}
+        self._failed_login_count = None
+        self._locale = None
+        
+    # Login
+    def login_with_password(self, password):
+        if password is None:
+            self.last_failed_login_time = datetime.now()
+            self.increment_failed_login_count()
+            raise AuthenticationLoginException( _("Login failed"),
+                _("Missing password: %(account_name)s") %
+                {'account_name' : self.account_name})
+             
+        # Don't let disabled users log in
+        if not self.is_enabled():
+            self.last_failed_login_time = datetime.now()
+            self.increment_failed_login_count()
+            raise AuthenticationLoginException( _("Login failed"),
+                _("Disabled user attempt to login: %(account_name)s") %
+                {'account_name' : self.account_name})
+                
+        # Don't let locked users log in
+        if self.is_locked():
+            self.last_failed_login_time = datetime.now()
+            self.increment_failed_login_count()
+            raise AuthenticationLoginException( _("Login failed"),
+                _("Locked user attempt to login: %(account_name)s") %
+                {'account_name' : self.account_name})
+                
+        # Don't let expired users log in
+        if self.is_expired():
+            self.last_failed_login_time = datetime.now()
+            self.increment_failed_login_count()
+            raise AuthenticationLoginException( _("Login failed"),
+                _("Expired user attempt to login: %(account_name)s") %
+                {'account_name' : self.account_name})
+                
+        self.logout()
+        if self.verify_password( password ):
+            self._logged_in = True
+            #ESAPI.http_utilities().change_session_identifier( ESAPI.current_request() )
+            #ESAPI.authenticator().set_current_user(self)
+            self.last_login_time = datetime.now()
+            #self.last_host_address = ESAPI.http_utilities().get_current_request().get_remote_addr()
+            self.logger.trace(Logger.SECURITY_SUCCESS, 
+                _("User logged in: %(account_name)s") %
+                {'account_name' : self.account_name})
+        else:
+            self._logged_in = False
+            self.last_failed_login_time = datetime.now()
+            self.increment_failed_login_count()
+            if self.failed_login_count >= ESAPI.security_configuration().get_allowed_login_attempts():
+                self.lock()
+            raise AuthenticationLoginException( _("Login failed"),
+                _("Incorrect password provided for %(account_name)s") %
+                {'account_name' : self.account_name})
+        
+    def logout(self):
+        #ESAPI.http_utilities().kill_cookie( ESAPI.current_request(), 
+        #    ESAPI.current_response(),
+        #    HTTPUtilities.REMEMBER_TOKEN_COOKIE_NAME )
+            
+        #session = ESAPI.current_request().get_session(False)
+        #remove me
+        session = None
+        
+        if session is not None:
+            self.remove_session(session)
+            session.invalidate()
+            
+        #ESAPI.http_utilities().kill_cookie( ESAPI.current_request(),
+        #    ESAPI.current_response(),
+        #    "SESSIONID" )
+        self._logged_in = False
+        self.logger.info(Logger.SECURITY_SUCCESS, _("Logout successful"))
+        #ESAPI.authenticator().set_current_user(User.ANONYMOUS)
+        
+    def is_logged_in(self):
+        return self._logged_in
+        
+    # Locale
+    def _get_locale(self):
+        return self._locale
+        
+    def _set_locale(self, locale):
+        self._locale = locale
+        
+    # Roles
+    def add_role(self, role):
+        """
+        If role is a string, it will be lower()'d.
+        """
+        if isinstance(role, str):
+            role = lower(role)
+            
+        if ESAPI.validator().is_valid_input("addRole",
+                            role, 
+                            "RoleName", 
+                            DefaultUser.MAX_ROLE_LENGTH, 
+                            False):
+            if role not in self._roles:
+                self._roles.append(role)
+                self.logger.info(Logger.SECURITY_SUCCESS,
+                     _("Role %(role_name)s added to %(account_name)s") %
+                     {'role_name' : role,
+                      'account_name' : self.account_name})
+            else:
+                # Role already assigned
+                pass
+        else:
+            raise AuthenticationAccountsException( _("Add role failed"),
+                _("Attempt to add invalid role %(role_name)s to %(account_name)s") %
+                {'role_name' : role,
+                 'account_name' : self.account_name})
+                              
+        
+    def remove_role(self, role):
+        if isinstance(role, str):
+            role = lower(role)
+            
+        try:
+            self._role.remove(role)
+            self.logger.trace(Logger.SECURITY_SUCCESS,
+                _("Role %(role_name)s removed from %(account_name)") %
+                {'role_name' : role,
+                 'account_name' : self.account_name})
+        except ValueError:
+            # Raise an exception?
+            pass
+        
+    def is_in_role(self, role):
+        if isinstance(role, str):
+            role = lower(role)
+            
+        return role in self._roles
+        
+    def _get_roles(self):
+        return tuple(self._roles)
+
+    def _set_roles(self, roles):
+        self._roles = list(roles)[:]
+      
+    def add_roles(self, roles):
+        for role in roles:
+            self.add_role(role)
+      
+    # Passwords
+    def verify_password(self, password):
+        #return ESAPI.authenticator().verify_password(self, password)
+        return None
+        
+    def change_password(self, old_password, new_password1, new_password2):
+        #ESAPI.authenticator().change_password(self, 
+        #    old_passowrd, 
+        #    new_password1, 
+        #    new_passord2)
+        return None
+        
+    def _get_last_password_change_time(self):
+        return self._last_password_change_time
+        
+    def _set_last_password_change_time(self, time):
+        self._last_password_change_time = time
+        self.logger.info( Logger.SECURITY_SUCCESS,
+            _("Set last password change time to %(time)s for %(account_name)s") %
+            {'time' : time,
+             'account_name' : self.account_name})
+        
+    # Enable/Disable
+    def disable(self):
+        self._enabled = False
+        self.logger.info( Logger.SECURITY_SUCCESS, 
+            _("Account disabled: %(account_name)s") %
+            {'account_name' : self.account_name})
+        
+    def enable(self):
+        self._enabled = True
+        self.logger.info( Logger.SECURITY_SUCCESS,
+            _("Account enabled: %(account_name)s") %
+            {'account_name' : self.account_name})
+        
+    def is_enabled(self):
+        return self._enabled
+        
+    # Account id
+    def _get_account_id(self):
+        return self._account_id
+        
+    # Account name
+    def _get_account_name(self):
+        return self._account_name
+        
+    def _set_account_name(self, name):
+        old = self.account_name
+        self._account_name = lower(name)
+        if old != None
+            if old == "":
+                old = "[nothing]"
+            self.logger.info( Logger.SECURITY_SUCCESS,
+                _("Account name changed from %(old)s to %(new)s") %
+                {'old' : old,
+                 'new' : self.account_name}
+        
+    # CSRF tokens
+    def _get_csrf_token(self):
+        return self._csrf_token
+        
+    def reset_csrf_token(self):
+        self._csrf_token = ESAPI.randomizer().get_random_string(8, 
+            DefaultEncoder.CHAR_ALPHANUMERICS)
+        return self.csrf_token
+        
+    # Expiration
+    def _get_expiration_time(self):
+        return self._expiration_time
+        
+    def _set_expiration_time(self, expiration_time):
+        self._expiration_time = expiration_time
+        self.logger.info( Logger.SECURITY_SUCCESS,
+            _("Account expiration time was set to %(time)s for %(account_name)s") %
+            {'time' : expiration_time,
+             'account_name' : self.account_name})
+        
+    def is_expired(self):
+        return self.expiration_time < datetime.now()
+        
+    # Failed logins
+    def get_failed_login_count(self):
+        return self._failed_login_count
+        
+    def increment_failed_login_count(self):
+        self._failed_login_count += 1
+        
+    def _get_last_failed_login_time(self):
+        return self._last_failed_login_time
+        
+    def _set_last_failed_login_time(self, time):
+        self._last_failed_login_time = time
+        logger.info( Logger.SECURITY_SUCCESS, 
+            _("Set last failed login time to %(time)s for %(user)s") %
+            {'time' : time,
+             'user' : self.account_name})
+        
+    # Host address
+    def _get_last_host_address(self):
+        if self._last_host_address is None:
+            return "unknown"
+        return self._last_host_address
+        
+    def _set_last_host_address(self, address):
+        if (self._last_host_address is not None and 
+            self._last_host_address != address):
+            raise AuthenticationHostException( _("Host change"),
+                _("User sessions just jumped from %(old)s to %(new)s") %
+                {'old' : self._last_host_address,
+                 'new' : address})
+        self._last_host_address = address
+        
+    # Login times
+    def _get_last_login_time(self):
+        return self._last_login_time
+        
+    def _set_last_login_time(self, time):
+        self._last_login_time = time
+        self.logger.info( Logger.SECURITY_SUCCESS,
+            _("Set last successful login time to %(time)s for %(account_name)s") %
+            {'time' : time,
+             'account_name' : self.account_name})
+       
+    # Screen names  
+    def _get_screen_name(self):
+        return self._screen_name
+        
+    def _set_screen_name(self, new_screen_name):
+        self._screen_name = new_screen_name
+        self.logger.info( Logger.SECURITY_SUCCESS,
+            _("ScreenName changed to %(new)s for %(account_name)s") %
+            {'new' : new_screen_name,
+             'account_name' : self.account_name})
+                            
+    # Session
+    def add_session(self, session):
+        self._session.append(session)
+        
+    def remove_session(self, session):
+        try:
+            self._session.remove(session)
+        except Exception:
+            pass
+        
+    def get_sessions(self):
+        return tuple(self._sessions)
+    
+    # Anonymous user
+    def is_anonymous(self):
+        raise NotImplementedError()
+        
+    # Timeouts
+    def is_session_absolute_timeout(self):
+        raise NotImplementedError()
+        
+    def is_session_timeout(self):
+        raise NotImplementedError()
+    
+    # Locking
+    def lock(self):
+        self._locked = True
+        self.logger.info( Logger.SECURITY_SUCCESS,
+            _("Account locked: %(account_name)s") %
+            {'account_name' : self.account_name})
+        
+    def unlock(self):
+        self._locked = False
+        self._failed_login_count = 0
+        self.logger.fail( Logger.SECURITY_SUCCESS,
+            _("Account unlocked: %(account_name)s") % 
+            {'account_name' : self.account_name})
+        
+    def is_locked(self):
+        return self._locked
+       
+    # Security event dictionary 
+    def get_event_dict(self):
+        raise NotImplementedError()
---
+++ esapi/test/test_user.py
@@ -0,0 +1,299 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@license: OWASP Enterprise Security API (ESAPI)
+     
+    This file is part of the Open Web Application Security Project (OWASP)
+    Enterprise Security API (ESAPI) project. For details, please see
+    U{http://www.owasp.org/index.php/ESAPI<http://www.owasp.org/index.php/ESAPI>}.
+
+    The ESAPI is published by OWASP under the BSD license. You should read and 
+    accept the LICENSE before you use, modify, and/or redistribute this software.
+    
+@summary: Test suite for User interface.
+@copyright: Copyright (c) 2009 - The OWASP Foundation
+@author: Craig Younkins (craig.younkins@owasp.org)
+"""
+
+# Todo
+# Implement any 'pass' methods
+# Regroup in same groups as User interface
+
+import unittest
+import inspect
+import time
+from datetime import datetime
+
+from esapi.core import ESAPI
+
+class UserTest(unittest.TestCase):
+
+    def __init__(self, test_name=""):
+        """       
+        @param test_name: the test name
+        """
+        unittest.TestCase.__init__(self, test_name)
+             
+    def create_test_user(self, username=None, password=None):
+        """
+        Creates a test user.
+        
+        @return: the test user
+        @raises AuthenticationException:
+        """
+        if username is None:
+            username = ESAPI.randomizer().get_random_string(8, DefaultEncoder. CHAR_ALPHANUMERICS)
+            
+        if password is None:
+            password = ESAPI.randomizer().get_random_string(8, DefaultEncoder.CHAR_ALPHANUMERICS)
+            
+        caller = inspect.stack()[2][3]
+        print (_("Creating user %(username)s for %(caller)s") %
+            {'username' : username,
+             'caller' : caller})
+        user = ESAPI.authenticator().create_user(username, password, password)
+        return user
+        
+    def test_add_role(self):
+        user = self.create_test_user()
+        role = "the role"
+        user.add_role(role)
+        self.assertTrue(user.is_in_role(role))
+        self.assertFalse(user.is_in_role("ridiculous"))
+        
+    def test_add_roles(self):
+        user = self.create_test_user()
+        roles = ['rolea', 'roleb']
+        user.add_roles(roles)
+        self.assertTrue(user.is_in_role('rolea'))
+        self.assertTrue(user.is_in_role('roleb'))
+        self.assertFalse(user.is_in_role('ridiculous'))
+        
+    def test_change_password(self):
+        instance = ESAPI.authenticator()
+        old_password = 'password12!@'
+        user = self.create_test_user(password=old_password)
+        print (_("Hash of %(old_password)s = %(hash)s") %
+            {'old_password' : old_password,
+             'hash' : instance.get_hashed_password(user)})
+        
+        password1 = "SomethingElse34#$"
+        user.change_password(old_password, password1, password1)
+        print (_("Hash of %(password)s = %(hash)s") %
+            {'password' : password1,
+             'hash' : instance.get_hashed_password(user)})
+        self.assertTrue(user.verify_password(password1))
+        self.assertFalse(user.verify_password(old_password))
+        
+        password2 = "YetAnother56%^"
+        user.change_password(password1, password2, password2)
+        print (_("Hash of %(password)s = %(hash)s") %
+            {'password' : password2,
+             'hash' : instance.get_hashed_password(user)})
+        self.assertTrue(user.verify_password(password2))
+        self.assertFalse(user.verify_password(password1))
+        
+        try: 
+            user.change_password(password2, password1, password1)
+            # Should not be able to re-use a password
+            self.fail()
+        except AuthenticationException:
+            pass
+            
+        self.assertFalse(user.verify_password("badpass"))
+     
+    def test_enable_disable(self):
+        user = self.create_test_user(password='password')
+        user.enable()
+        self.assertTrue(user.is_enabled())
+        user.disable()
+        self.assertFalse(user.is_enabled())
+        
+    def test_failed_login_lockout(self):
+        pass
+    
+    def test_account_name(self):
+        user = self.create_test_user(username='myuser')
+        account_name = 'new name'
+        user.account_name = account_name
+        self.assertTrue(account_name == user.account_name)
+        self.assertFalse("ridiculous" == use.account_name)
+        
+    def test_last_failed_login_time(self):
+        user = self.create_test_user()
+        
+        try:
+            user.login_with_password("ridiculous")
+        except:
+            pass
+            
+        time1 = user.last_failed_login_time
+        time.sleep(0.01)
+        try:
+            user.login_with_password("ridiculous")
+        except:
+            pass
+
+        time2 = user.last_failed_login_time
+        self.assertFalse(time1 == time2)
+        self.assertTrue(time1 < time2)
+        
+    def test_last_login_time(self):
+        user = self.create_test_user(password='testpass')
+        user.verify_password('testpass')
+        time1 = user.last_login_time
+        time.sleep(0.01)
+        user.verify_password('testpass')
+        time2 = user.last_login_time
+        self.assertTrue(time1 < time2)
+        
+    def test_last_password_change_time(self):
+        old_password = 'pass1'
+        user = self.create_test_user(password=old_password)
+        time1 = user.last_password_change_time
+        time.sleep(0.01)
+        new_password = 'pass2'
+        user.change_password(old_password, new_password, new_password)
+        time2 = user.last_password_change_time
+        self.assertTrue(time1 < time2)
+        
+    def test_get_roles(self):
+        user = self.create_test_user()
+        role = 'ADMIN'
+        user.add_role(role)
+        roles = user.roles
+        self.assertTrue(len(roles) > 0)
+        self.assertTrue(role in roles)
+        
+    def test_remove_role(self):
+        user = self.create_test_user()
+        role = 'the role'
+        user.add_role(role)
+        self.assertTrue(user.is_in_role(role))
+        self.remove_role(role)
+        self.assertFalse(user.is_in_role(role))
+        
+    def test_screen_name(self):
+        user = self.create_test_user()
+        screen_name = 'c00lk1d'
+        user.screen_name = screen_name
+        self.assertEquals(screen_name, user.screen_name)
+        self.assertFalse('ridiculous' == user.screen_name)
+        
+    def test_get_sessions(self):
+        pass
+           
+    def test_add_sessions(self):
+        pass
+        
+    def test_remove_sessions(self):
+        pass
+        
+    def test_increment_failed_login_count(self):
+        user = self.create_test_user()
+        user.enable()
+        self.assertEquals(0, user.failed_login_count)
+        
+        try:
+            user.login_with_password("ridiculous")
+        except:
+            pass
+            
+        self.assertEquals(1, user.failed_login_count)
+        
+        try:
+            user.login_with_password("ridiculous")
+        except:
+            pass
+            
+        self.assertEquals(2, user.failed_login_count)
+        
+    def test_is_enabled(self):
+        user = self.create_test_user()
+        user.disable()
+        self.assertFalse(user.is_enabled())
+        user.enable()
+        self.assertTrue(user.is_enabled())
+        
+    def test_is_in_role(self):
+        user = self.create_test_user()
+        role = "testrole"
+        self.assertFalse(user.is_in_role(role))
+        user.add_role(role)
+        self.assertTrue(user.is_in_role(role))
+        self.assertFalse(user.is_in_role("ridiculous"))
+        
+    def test_set_roles(self):
+        user = self.create_test_user()
+        user.add_role('user')
+        user.roles = ['rolea', 'roleb']
+        self.assertFalse(user.is_in_role('user'))
+        self.assertTrue(user.is_in_role('rolea'))
+        self.assertTrue(user.is_in_role('roleb'))
+        self.assertFalse(user.is_in_role('ridiculous'))
+        
+    def test_locking(self):
+        user = self.create_test_user()
+        user.lock()
+        self.assertTrue(user.is_locked())
+        user.unlock()
+        self.assertFalse(user.is_locked())
+        
+    def test_is_session_absolute_timeout(self):
+        pass
+        
+    def test_is_session_timeout(self):
+        pass
+        
+    def test_login_with_password(self):
+        user = self.create_test_user(password='password')
+        user.enable()
+        user.login_with_password('password')
+        self.assertTrue(user.is_logged_in())
+        
+        user.logout()
+        self.assertFalse(user.is_logged_in())
+        self.assertFalse(user.is_locked())
+        
+        for i in range(3):
+            try:
+                user.login_with_password('password')
+            except:
+                pass
+            self.assertFalse(user.is_logged_in())
+            
+        self.assertTrue(user.is_locked())
+        user.unlock()
+        self.assertEquals(user.failed_login_count, 0)
+            
+    def test_logout(self):
+        password = 'test123'
+        user = self.create_test_user(password=password)
+        user.enable()
+        user.login_with_password(password)
+        self.assertTrue(user.is_logged_in())
+        user.logout()
+        self.assertFalse(user.is_logged_in())
+        
+    def test_reset_csrf(self):
+        user = self.create_test_user()
+        token1 = user.reset_csrf_token()
+        token2 = user.reset_csrf_token()
+        self.assertFalse(token1 == token2)
+        
+    def test_expiration_time(self):
+        user = self.create_test_user()
+        
+        # Date in past, should expire
+        user.expiration_time = datetime.min
+        self.assertTrue( user.is_expired() )
+        
+        # Date in future, should not expire
+        user.expiration_time = datetime.max
+        self.assertFalse( user.is_expired() )
+        
+           
+if __name__ == "__main__":
+    unittest.main()
+
